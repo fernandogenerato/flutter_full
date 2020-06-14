6853b2f25c5d1655f93d9f4c8a3070c50a4fffe6
Fernando <generatof@gmail.com>
2020-06-14 01:36:51 -0300

    Adicionar imagens via rede ou assets; Alinhamento dos filhos do Widget Column.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,10 +9,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
+        theme: ThemeData(
+          primaryColor: Colors.grey[900],
+          accentColor: Colors.lightGreenAccent[100],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.lightGreenAccent[100],
+          ),
         ),
+        home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme
+                  .of(context)
+                  .primaryColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                  Text(
+                    'Contatos',
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
