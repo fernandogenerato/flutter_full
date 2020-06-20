40e0ad6190d9615738515aa382851772a800737c
Fernando <generatof@gmail.com>
2020-06-20 18:44:49 -0300

    Configuração de timeout para todas as utilizacoes do client

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -4,4 +4,6 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 const String base_url = 'http://192.168.0.67:8080/transactions';
 final Client client =
-    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()],
+    requestTimeout: Duration(seconds: 5),
+    );
